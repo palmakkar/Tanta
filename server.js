1b27d4c35f5a30e62bfcccec550a7de91ce3c83f
Pal Makkar <pmakkar97@gmail.com>
2016-09-13 19:25:46 -0700

    added connect as dependency

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-var server = app.listen((process.env.PORT || 8080), function () {
-  var port = server.address().port;
-  console.log('Listening at port ' + port);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
 });
